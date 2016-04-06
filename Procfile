1cee47a262a91da7997e6380bfff6f7b9fd8d6ef
terrelewis <terrellewis@hotmail.com>
2016-04-06 17:37:53 +0530

    Reverted changes for heroku

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: gunicorn plateup.wsgi
--- tinderstomach/settings.py
+++ tinderstomach/settings.py
@@ -127,28 +127,3 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = '/home//terrelewis/tinderstomach/tinderstomach/media/'
 MEDIA_URL = 'http://127.0.0.1:8000/rest_picker/media/'
 
-import dj_database_url
-DATABASES['default'] = dj_database_url.config()
-
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-
-ALLOWED_HOSTS = ['*']
-
-DEBUG = False
-
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-try:
-    from .local_settings import *
-except ImportError:
-    pass
